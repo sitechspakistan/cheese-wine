fe5d269206519d659e910fbf419bd3190b95f14d
seoiniziosolutions <seo.iniziosolutions@gmail.com>
2026-05-23 00:27:15 +0500

    Updated

--- src/app/book-now/page.js
+++ src/app/book-now/page.js
@@ -9,33 +9,22 @@ export default function BookingPage() {
   return (
     <>
       <HeroSub heading="Book Now" overlay="true" />
-      <div className="w-full mb-12 max-w-7xl mx-auto mt-25 overflow-hidden">
-        <style>{`
-        #bookingFrame::-webkit-scrollbar {
-          display: none;
-          }
-          #bookingFrame {
-            scrollbar-width: none;
-            -ms-overflow-style: none;
-            }
-            `}</style>
-        <iframe
-          src="https://admin.hostsync.io/booking2.php?propid=322695&amp;referer=iframe"
-          width={"100%"}
-          height={"1000px"}
-          className="overflow-hidden border-none"
-          id="bookingFrame"
-        >
-          <p>
-            <a
-              href="https://admin.hostsync.io/booking2.php?propid=322695&amp;referer=iframe"
-              title="Book Now"
-            >
-              Book Now
-            </a>
-          </p>
-        </iframe>
-      </div>
+      {/* <div className=" max-w-7xl mx-auto  overflow-hidden"> */}
+      <iframe
+        src="https://admin.hostsync.io/booking2.php?propid=322695&amp;referer=iframe"
+        width={"100%"}
+        height={"1000px"}
+        className="overflow-hidden border-none -mt-12 relative"
+        id="bookingFrame"
+      >
+        {/* <a
+            href="https://admin.hostsync.io/booking2.php?propid=322695&amp;referer=iframe"
+            title="Book Now"
+          >
+            Book Now
+          </a> */}
+      </iframe>
+      {/* </div> */}
     </>
   );
 }
