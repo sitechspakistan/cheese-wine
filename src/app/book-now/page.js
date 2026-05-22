c2b604a9cdb9669625adb1569dbd2af3cf5206f6
seoiniziosolutions <seo.iniziosolutions@gmail.com>
2026-05-22 22:57:03 +0500

    Updated

--- src/app/book-now/page.js
+++ src/app/book-now/page.js
@@ -1,35 +1,41 @@
+'use client";';
+import HeroSub from "../components/sections/Hero-2";
+
 export const metadata = {
   title: "Book Now | Cheese & Wine",
   // description: "...",
 };
 export default function BookingPage() {
   return (
-    <div className="w-full mb-12 max-w-7xl mx-auto mt-25 overflow-hidden">
-      <style>{`
+    <>
+      <HeroSub heading="Book Now" overlay="true" />
+      <div className="w-full mb-12 max-w-7xl mx-auto mt-25 overflow-hidden">
+        <style>{`
         #bookingFrame::-webkit-scrollbar {
           display: none;
-        }
-        #bookingFrame {
-          scrollbar-width: none;
-          -ms-overflow-style: none;
-        }
-      `}</style>
-      <iframe
-        src="https://admin.hostsync.io/booking2.php?propid=322695&amp;referer=iframe"
-        width={"100%"}
-        height={"1000px"}
-        className="overflow-hidden border-none"
-        id="bookingFrame"
-      >
-        <p>
-          <a
-            href="https://admin.hostsync.io/booking2.php?propid=322695&amp;referer=iframe"
-            title="Book Now"
-          >
-            Book Now
-          </a>
-        </p>
-      </iframe>
-    </div>
+          }
+          #bookingFrame {
+            scrollbar-width: none;
+            -ms-overflow-style: none;
+            }
+            `}</style>
+        <iframe
+          src="https://admin.hostsync.io/booking2.php?propid=322695&amp;referer=iframe"
+          width={"100%"}
+          height={"1000px"}
+          className="overflow-hidden border-none"
+          id="bookingFrame"
+        >
+          <p>
+            <a
+              href="https://admin.hostsync.io/booking2.php?propid=322695&amp;referer=iframe"
+              title="Book Now"
+            >
+              Book Now
+            </a>
+          </p>
+        </iframe>
+      </div>
+    </>
   );
 }
